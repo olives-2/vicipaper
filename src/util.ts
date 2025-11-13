ff2f36905b043cee5f9a66deec16266f7662c9a3
Matis Olives <matisolives@gmail.com>
2025-11-13 16:43:12 +0100

    made last minute types file, fixed import...

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,7 @@
 import path from 'node:path';
 import fs from "node:fs";
 import os from "os";
-import { type Wallpaper } from './wallpaper-setter';
+import { type Wallpaper } from './types';
 
 /**
  * File types supported by awww (previously swww)
